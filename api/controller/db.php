a0a792b132a0c546ee8e1dae337769390f767fcc
Aung Kyaw Minn <aungkyawminn.mg@gmail.com>
2020-09-22 20:26:53 +0630

    new db version

--- api/controller/db.php
+++ api/controller/db.php
@@ -5,7 +5,7 @@ class DB {
 
     public static function connectWriteDB() {
         if(self::$writeDBConnection === null) {
-            self::$writeDBConnection = new PDO('mysql:host=localhost;dbname=herp;charset=utf8', 'root', 'root');
+            self::$writeDBConnection = new PDO('mysql:host=localhost;port=3309;dbname=herp;charset=utf8', 'root', 'root');
             self::$writeDBConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             self::$writeDBConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         }
@@ -14,7 +14,7 @@ class DB {
 
     public static function connectReadDB() {
         if(self::$readDBConnection === null) {
-            self::$readDBConnection = new PDO('mysql:host=localhost;dbname=herp;charset=utf8', 'root', 'root');
+            self::$readDBConnection = new PDO('mysql:host=localhost;port=3309;dbname=herp;charset=utf8', 'root', 'root');
             self::$readDBConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             self::$readDBConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         }
