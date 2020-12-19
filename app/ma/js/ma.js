0ba793b0047b4b407bab47e78266773d239c6d0a
yeaung <yeaung@DESKTOP-4MELPK3.ya>
2020-12-19 18:21:05 +0630

    first release

--- app/ma/js/ma.js
+++ app/ma/js/ma.js
@@ -148,24 +148,24 @@ function showMenu() {
 	html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="patients.html" ><i class="mdi  mdi-needle"></i><span class="hide-menu">Patients</span></a></li>';
 	html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="opdroom.html" ><i class="mdi  mdi-home"></i><span class="hide-menu">OPD Rooms</span></a></li>';
 	html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="medical_record.html" ><i class="mdi  mdi-book-plus"></i><span class="hide-menu">Medical Record</span></a></li>';
-	html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="investigation_request.html" ><i class="mdi  mdi-book-plus"></i><span class="hide-menu">Investigation Request</span></a></li>';
-	html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="medical_record_prescription.html" ><i class="mdi  mdi-book-plus"></i><span class="hide-menu">Prescription</span></a></li>';
+	// html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="investigation_request.html" ><i class="mdi  mdi-book-plus"></i><span class="hide-menu">Investigation Request</span></a></li>';
+	// html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="medical_record_prescription.html" ><i class="mdi  mdi-book-plus"></i><span class="hide-menu">Prescription</span></a></li>';
 	html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="employee.html" ><i class="mdi mdi-account-multiple"></i><span class="hide-menu">Employees</span></a></li>';
 	html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="department.html" ><i class="mdi  mdi-hospital-building"></i><span class="hide-menu">Departments</span></a></li>';
 	html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="position.html" ><i class="mdi  mdi-hospital-building"></i><span class="hide-menu">Positions</span></a></li>';
-	html_menu += lev < 6 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="user.html" ><i class="mdi mdi-account"></i><span class="hide-menu">Users</span></a></li>';
-	html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="diagnosis_report.html" ><i class="mdi  mdi-file-chart"></i><span class="hide-menu">Diagnosis Report</span></a></li>';
-	html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="suppliers.html" ><i class="mdi  mdi-truck"></i><span class="hide-menu">Suppliers</span></a></li>';
-	html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="billing.html" ><i class="mdi  mdi-credit-card"></i><span class="hide-menu">Billing</span></a></li>';
-	html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="pharmacy_items.html" ><i class="mdi  mdi-pill"></i><span class="hide-menu">Pharmacy Items</span></a></li>';
-	html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="pharmacy_inventory.html" ><i class="mdi  mdi-pill"></i><span class="hide-menu">Pharmacy Inventory</span></a></li>';
-	html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="pharmacy_sale.html" ><i class="mdi  mdi-pill"></i><span class="hide-menu">Pharmacy Sale</span></a></li>';
-	html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="pharmacy_purchase.html" ><i class="mdi  mdi-pill"></i><span class="hide-menu">Pharmacy Purchase</span></a></li>';
-	html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="salary.html" ><i class="mdi  mdi-hospital-building"></i><span class="hide-menu">Salary</span></a></li>';
-	html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="attadance.html" ><i class="mdi  mdi-hospital-building"></i><span class="hide-menu">Attadance</span></a></li>';
-	html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="payroll.html" ><i class="mdi  mdi-hospital-building"></i><span class="hide-menu">Payroll</span></a></li>';
-	html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="leave_request.html" ><i class="mdi  mdi-hospital-building"></i><span class="hide-menu">Leave Requests</span></a></li>';
-	html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="leave_type.html" ><i class="mdi  mdi-hospital-building"></i><span class="hide-menu">Leave Types</span></a></li>';
+	// html_menu += lev < 6 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="user.html" ><i class="mdi mdi-account"></i><span class="hide-menu">Users</span></a></li>';
+	// html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="diagnosis_report.html" ><i class="mdi  mdi-file-chart"></i><span class="hide-menu">Diagnosis Report</span></a></li>';
+	// html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="suppliers.html" ><i class="mdi  mdi-truck"></i><span class="hide-menu">Suppliers</span></a></li>';
+	// html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="billing.html" ><i class="mdi  mdi-credit-card"></i><span class="hide-menu">Billing</span></a></li>';
+	// html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="pharmacy_items.html" ><i class="mdi  mdi-pill"></i><span class="hide-menu">Pharmacy Items</span></a></li>';
+	// html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="pharmacy_inventory.html" ><i class="mdi  mdi-pill"></i><span class="hide-menu">Pharmacy Inventory</span></a></li>';
+	// html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="pharmacy_sale.html" ><i class="mdi  mdi-pill"></i><span class="hide-menu">Pharmacy Sale</span></a></li>';
+	// html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="pharmacy_purchase.html" ><i class="mdi  mdi-pill"></i><span class="hide-menu">Pharmacy Purchase</span></a></li>';
+	// html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="salary.html" ><i class="mdi  mdi-hospital-building"></i><span class="hide-menu">Salary</span></a></li>';
+	// html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="attadance.html" ><i class="mdi  mdi-hospital-building"></i><span class="hide-menu">Attadance</span></a></li>';
+	// html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="payroll.html" ><i class="mdi  mdi-hospital-building"></i><span class="hide-menu">Payroll</span></a></li>';
+	// html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="leave_request.html" ><i class="mdi  mdi-hospital-building"></i><span class="hide-menu">Leave Requests</span></a></li>';
+	// html_menu += lev < 5 ? '':'<li class="sidebar-item "><a class="sidebar-link sidebar-link" href="leave_type.html" ><i class="mdi  mdi-hospital-building"></i><span class="hide-menu">Leave Types</span></a></li>';
 
 
 
